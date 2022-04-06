a42ddfc6bd5c9c5cbe83251420269519113af272
Nourelhouda Taroudi <nourelhoudataroudi@gmail.com>
2022-04-06 21:32:54 +0000

    added lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
+import { PageNotFoundComponent } from './shared/errors/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'auth' , 
+    loadChildren: () => 
+    import('./features/auth/auth.module').then(m => m.AuthModule) 
+  },
+  { 
+    path: '' , 
+    children:[
+      {
+        path:'',
+        loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: 'home', 
+        redirectTo:''
+      }
+    ] 
+  },
+  { 
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
