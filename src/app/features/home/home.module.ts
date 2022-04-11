5bf1f6656871a5a3d4091051a0b5a5d177ee2141
Oumaima Mt <oumaimamt@MacBook-Pro-de-Oumaima.local>
2022-04-11 10:26:31 +0000

    desgined home

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from './../../angular-material/angular-material.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    AngularMaterialModule,
   ]
 })
 export class HomeModule { }
