0558d000392cdfeaa96be32a84d9b2ecd603da66
Nourelhouda Taroudi <nourelhoudataroudi@gmail.com>
2022-05-22 15:35:30 +0100

    added design login

--- src/app/profile/profil-detail/profil-detail.component.ts
+++ src/app/profile/profil-detail/profil-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';  
 import { Post } from '../models/post';
-import { PostService } from '../../services/post.service';
+import { PostService } from '../../core/services/post.service';
 
 @Component({
   selector: 'app-profil-detail',
